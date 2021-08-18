62fb063aec9d22f3b79fe4d642087863b3b5237f
Avi Kaura <avikaura20@gmail.com>
2021-08-18 17:37:08 -0400

    My Portfolio webpage has been created using JS, CSS

---
+++ js/style.js
@@ -0,0 +1,32 @@
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
+
+
+/*----------- TOGGLE MENU---------- */
+
+//--------Toggle dropdwon TOP-BAR (SMALLEST VIEW PORT)
+
+function myFunction() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+      menu.style.display = "none";
+    } else {
+      menu.style.display = "block";
+    }
+  }
